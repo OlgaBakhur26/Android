ccf373a0b299318009cf0456c9618825de2a9fe2
Olga Bakhur <olgabakhur123@gmail.com>
2020-08-01 14:09:43 +0300

    HW 1-2

---
+++ HW/app/src/main/java/com/example/flags/FlagActivity.java
@@ -0,0 +1,14 @@
+package com.example.flags;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flag);
+    }
+}
