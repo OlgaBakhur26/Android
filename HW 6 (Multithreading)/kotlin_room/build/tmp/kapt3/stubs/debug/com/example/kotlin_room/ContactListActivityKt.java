6596e15b2189775f895efc867f349094efb14619
Olga Bakhur <olgabakhur123@gmail.com>
2020-09-25 20:29:51 +0300

    delete

--- HW 6 (Multithreading)/kotlin_room/build/tmp/kapt3/stubs/debug/com/example/kotlin_room/ContactListActivityKt.java
+++
@@ -1,9 +0,0 @@
-package com.example.kotlin_room;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\b\n\u0000\n\u0002\u0010\u000e\n\u0000\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0080T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0002"}, d2 = {"CONTACT_FOR_EDITING", "", "kotlin_room_debug"})
-public final class ContactListActivityKt {
-    @org.jetbrains.annotations.NotNull
-    public static final java.lang.String CONTACT_FOR_EDITING = "CONTACT_FOR_EDITING";
-}
