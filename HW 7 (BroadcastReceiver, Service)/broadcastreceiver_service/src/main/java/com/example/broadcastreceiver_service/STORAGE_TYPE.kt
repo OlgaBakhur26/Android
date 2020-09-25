50b199a0f3ca4a962d412c8dac1f39acf5625a53
Olga Bakhur <olgabakhur123@gmail.com>
2020-09-25 13:54:45 +0300

    delete

--- HW 7 (BroadcastReceiver, Service)/broadcastreceiver_service/src/main/java/com/example/broadcastreceiver_service/STORAGE_TYPE.kt
+++
@@ -1,6 +0,0 @@
-package com.example.broadcastreceiver_service
-
-enum class STORAGE_TYPE {
-    INTERNAL,
-    EXTERNAL
-}
--- HW 7 (BroadcastReceiver, Service)/customview/src/main/java/com/example/customview/NOTIFICATION_TYPE.java
+++
@@ -1,6 +0,0 @@
-package com.example.customview;
-
-public enum NOTIFICATION_TYPE {
-    SNACKBAR,
-    TOAST;
-}
